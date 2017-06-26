98ea4b7ca305daa569672d82c35149974a73fd11
101piano <771594535@qq.com>
2017-06-26 22:32:34 +0800

    判断数据类型

--- src/components/ResumeEditor.vue
+++ src/components/ResumeEditor.vue
@@ -13,7 +13,16 @@
     </nav>
     <ol class="panels">
       <li v-for="item in resume.config" v-show="item.field === selected">
-        <div class="resumeField" v-for="(value,key) in resume[item.field]">
+        <div v-if="resume[item.field] instanceof Array">
+          <div class="subitem" v-for="subitem in resume[item.field]">
+            <div class="resumeField" v-for="(value,key) in subitem">
+              <label> {{key}}</label>
+              <input type="text" :value="value">
+            </div>
+            <hr>
+          </div>
+        </div>
+        <div v-else class="resumeField" v-for="(value,key) in resume[item.field]">
           <label> {{key}}</label>
           <input type="text" v-model="resume[item.field][key]">
         </div>
@@ -42,11 +51,26 @@
             城市: '',
             应聘职位: ''
           },
-          'work history': [],
-          education: [],
-          projects: [],
-          awards: [],
-          contacts: []
+          'work history': [
+            { company: 'AL',content: '我的第二份工作是' },
+            { company: 'TX',content: '我的第一份工作是' }
+          ],
+          education: [
+            { school: 'AL',content: '文字' },
+            { school: 'TX',content: '文字' }
+          ],
+          projects: [
+            { name: 'project A',content: '文字' },
+            { name: 'project B',content: '文字' }
+          ],
+          awards: [
+            { name: 'awards A',content: '文字' },
+            { name: 'awards B',content: '文字' }
+          ],
+          contacts: [
+            { contact: 'phone',content: '文字' },
+            { contact: 'qq',content: '文字' }
+          ]
         }
       }
     }
@@ -106,7 +130,11 @@
     }
     ol {
       list-style: none;
-    }
-    
+    }    
+  }
+  hr {
+    border: none;
+    border-top: 1px solid #ddd;
+    margin: 24px 0;
   }
 </style>
